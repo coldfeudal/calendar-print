c6001b84d25c506cec9ab4f4529e2faabd30d702
coldfeudal <27438739+coldfeudal@users.noreply.github.com>
2026-09-19 15:01:06 +0300

    finished visuals

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,82 @@
 <script lang="ts">
+    const dow = [
+        "ПН",
+        "ВТ",
+        "СР",
+        "ЧТ",
+        "ПТ",
+        "СБ",
+        "ВС",
+    ]
 
+    const months = [
+        "Январь",
+        "Февраль",
+        "Март",
+        "Апрель",
+        "Май",
+        "Июнь",
+        "Июль",
+        "Август",
+        "Сентябрь",
+        "Октябрь",
+        "Ноябрь",
+        "Декабрь",
+    ]
+
+    const year = 2026
 </script>
 
 <svelte:head>
-    <title>Index</title>
+    <title>Календарь</title>
 </svelte:head>
 
 <!-- eslint-disable-next-line @typescript-eslint/no-unused-vars -->
-{#each Array(9) as _}
+{#each months as month}
     <div class="a4 ff f-col">
         <div class="title-line ffb">
-            <div class="display month">АВГУСТ</div>
-            <div class="display year">2026</div>
+            <div class="display month">{month}</div>
+            <div class="display year">{year}</div>
         </div>
         <div class="calendar">
-
+            <div class="calendar-body ff f-col">
+                <div class="day-names ff">
+                    {#each dow as day}
+                        <div class="day-name">
+                            <div class="name-text">{day}</div>
+                        </div>
+                    {/each}
+                </div>
+                <div class="days">
+                    {#each Array(42) as _}
+                        <div class="day">
+                            <div class="day-number">1</div>
+                            <div class="day-lines ffb f-col">
+                                {#each Array(6) as _}
+                                    <div class="day-line" />
+                                {/each}
+                            </div>
+                        </div>
+                    {/each}
+                </div>
+            </div>
         </div>
     </div>
 {/each}
 
 <style lang="scss">
+
+    $border: 1px solid black;
+    @mixin border {
+        border-bottom: $border;
+        border-right: $border;
+    }
+
+    @mixin thin-title {
+        transform: scaleX(85%);
+        font-weight: 700;
+    }
+
     .a4 {
         width: 297mm;
         height: 210mm;
@@ -30,17 +87,17 @@
 
         .title-line {
             font-size: 60px;
-            font-weight: 700;
             font-size: 60px;
             padding-inline: 235px;
             padding-block: 33px 21px;
             flex-shrink: 0;
 
             .display {
-                transform: scaleX(85%);
+                @include thin-title;
 
                 &.month {
                     transform-origin: left center;
+                    text-transform: uppercase;
                 }
 
                 &.year {
@@ -54,6 +111,64 @@
             width: 100%;
             padding: 33px;
             padding-top: 0;
+
+            &-body {
+                height: 100%;
+                width: 100%;
+                border-top: $border;
+                border-left: $border;
+            }
+
+            .day-names {
+                font-size: 17px;
+
+                .day-name {
+                    flex: 1;
+                    text-align: center;
+                    background-color: #dbdbdb;
+                    padding-block: 2px 1px;
+                    @include border;
+
+                    .name-text {
+                        @include thin-title;
+                    }
+                }
+            }
+
+            .days {
+                width: 100%;
+                height: 100%;
+                display: grid;
+                grid-template-columns: repeat(7, 1fr);
+
+                .day {
+                    @include border;
+                    position: relative;
+
+                    .day-number {
+                        @include thin-title;
+                        transform-origin: left center;
+                        font-size: 16px;
+                        position: absolute;
+                        left: 4px;
+                        top: 1px;
+                    }
+
+                    .day-lines {
+                        width: 100%;
+                        height: 100%;
+                        gap: 10px;
+                        padding-inline: 3px;
+                        padding-block: 21px 13px;
+
+                        .day-line {
+                            height: 1px;
+                            width: 100%;
+                            background-color: #998f86;
+                        }
+                    }
+                }
+            }
         }
     }
 
