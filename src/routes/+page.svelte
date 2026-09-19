fe4132222305ac635e21c38f7170efe07634c5e6
coldfeudal <27438739+coldfeudal@users.noreply.github.com>
2026-09-19 14:14:07 +0300

    started project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,41 @@
 <script lang="ts">
-    
+
 </script>
 
 <svelte:head>
     <title>Index</title>
 </svelte:head>
 
-<div class="page ffb">
-    Test
-</div>
+{#each Array(9) as _}
+    <div class="a4">
+
+    </div>
+{/each}
 
 <style lang="scss">
-    .page {
-        
+    .a4 {
+        width: 297mm;
+        height: 210mm;
+        outline: 2px dashed #000;
+        outline-offset: -2px;
+        break-after: page;
+        margin: 20px;
+    }
+
+    @page {
+        size: A4;
+        margin: 0;
+    }
+
+    @media print {
+        :global(html, body) {
+            width: 297mm;
+            height: 210mm;
+        }
+
+        .a4 {
+            outline: none;
+            margin: 0;
+        }
     }
 </style>
